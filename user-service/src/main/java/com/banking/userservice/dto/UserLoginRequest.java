7691143a2e04df9e6395399f96d787a1010d2bbf
joannawalach1 <joanna.walach@op.pl>
2025-06-28 02:37:12 +0200

    user-service jwt fixes

--- user-service/src/main/java/com/banking/userservice/dto/UserLoginRequest.java
+++ user-service/src/main/java/com/banking/userservice/dto/UserLoginRequest.java
@@ -1,8 +1,12 @@
 package com.banking.userservice.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserLoginRequest {
     private String username;
     private String password;
