bc602268fffdf492c5eac0d60e1faa516c9702e5
joannawalach1 <joanna.walach@op.pl>
2025-06-28 03:01:14 +0200

    user-service jwt fixes

--- account-service/src/main/java/com/banking/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/com/banking/accountservice/AccountServiceApplication.java
@@ -3,9 +3,11 @@ package com.banking.accountservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableMongoRepositories
 public class AccountServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccountServiceApplication.class, args);
