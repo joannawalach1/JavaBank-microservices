238e64e92a496f4703595b98c3d3c1df45be2321
joannawalach1 <joanna.walach@op.pl>
2025-07-26 22:54:27 +0200

    refactor

--- transaction-service/integration/java/com.banking/TransactionServiceIntegrationTest.java
+++ transaction-service/integration/java/com.banking/TransactionServiceIntegrationTest.java
@@ -1,7 +1,6 @@
 package com.banking;
 
 import com.banking.transactionservice.TransactionServiceApplication;
-import com.banking.transactionservice.configuration.CassandraConfig;
 import com.banking.transactionservice.dto.TransactionCreateRequest;
 import com.banking.transactionservice.dto.TransactionResponseDto;
 import org.junit.jupiter.api.Test;
@@ -11,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 
 import java.math.BigDecimal;
 import java.util.Objects;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -21,21 +21,22 @@ public class TransactionServiceIntegrationTest extends BaseIntegrationTest {
     @Test
     public void should_create_new_transaction() {
         // 1. Tworzymy nową transakcję
-        TransactionCreateRequest transactionCreateRequest = new TransactionCreateRequest(123L, "456", 789L, new BigDecimal("500.00"), "USD", "TRANSFER");
+        TransactionCreateRequest transactionCreateRequest = new TransactionCreateRequest(123L, "13", 789L, new BigDecimal("500.00"), "USD", "TRANSFER");
         ResponseEntity<TransactionResponseDto> registerResponse = restTemplate.postForEntity(
                 "/api/transactions",
                 transactionCreateRequest,
                 TransactionResponseDto.class
         );
 
-        assertEquals(HttpStatus.CREATED, registerResponse.getStatusCode());
+
+        assertEquals(HttpStatus.OK, registerResponse.getStatusCode());
         assertNotNull(registerResponse.getBody());
         TransactionResponseDto registeredTransaction = registerResponse.getBody();
-        assertEquals(456L, registerResponse.getBody().getAccountId());
+        assertEquals("13", registerResponse.getBody().getAccountId());
         assertEquals(123L, registerResponse.getBody().getUserId());
 
         // 2. Pobranie transakcji po ID
-        String id = registeredTransaction.getAccountId();
+        UUID id = registeredTransaction.getId();
         ResponseEntity<TransactionResponseDto> getByAccountIdResponse = restTemplate.getForEntity(
                 "/api/transactions/" + id,
                 TransactionResponseDto.class
